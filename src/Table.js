ab0a8feae7f3ad79630879eb1718037878c28ba7
Renan Franca <renanofranca@hotmail.com>
2022-05-21 18:54:08 -0300

    exclusão medicamento

--- src/Table.js
+++ src/Table.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
+import axios from 'axios';
 import { useTable } from "react-table";
 
 export default function Table({ columns, data }) {
+
   // Use the useTable Hook to send the columns and data to build the table
   const {
     getTableProps, // table props from react-table
@@ -13,7 +15,6 @@ export default function Table({ columns, data }) {
     columns,
     data
   });
-
   /* 
     Render the UI for your table
     - react-table doesn't have UI, it's headless. We just need to put the react-table props from the Hooks, and it will do its magic automatically
@@ -35,8 +36,15 @@ export default function Table({ columns, data }) {
           return (
             <tr className="border border-slate-400"{...row.getRowProps()}>
               {row.cells.map(cell => {
-                if(cell.column.id == "Id"){
-                  return <td className="border border-slate-400" {...cell.getCellProps()} ><button type="button" class="btn btn-danger">Secondary</button>
+                if(cell.column.id === "Id"){
+                  return <td className="border border-slate-400" {...cell.getCellProps()} >
+                    <div>
+                      <button onClick={()=>deleteMedicamento(cell.row.original.Id)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
+                          <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                        </svg>
+                      </button>
+                    </div>
                   </td>;
                 }
                 else{
@@ -52,6 +60,11 @@ export default function Table({ columns, data }) {
   );
 }
 
-function excludeMed(id){
-  console.log("teste");
+function deleteMedicamento(id){
+  axios.delete('http://localhost:8080/api/medicamento', {
+    params: {
+      Id: id
+    }
+  });
+  window.location.reload()
 }
